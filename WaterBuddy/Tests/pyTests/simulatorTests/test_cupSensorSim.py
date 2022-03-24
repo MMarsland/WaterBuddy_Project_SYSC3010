12a70c60bfd625db2685a2dc296c7d330a4263e4
Nick Milani <nickmilani@cmail.carleton.ca>
2022-03-24 00:02:20 -0400

    test: Add cupSensorSim test

---
+++ WaterBuddy/Tests/pyTests/simulatorTests/test_cupSensorSim.py
@@ -0,0 +1,26 @@
+import sys, time
+sys.path.append('../../../')
+sys.path.append('../../../Simulators')
+from fillSystemSim import CupSensorSim
+
+def test_init():
+    cupSensor = CupSensorSim()
+    assert(not cupSensor == None)
+
+def test_CupSensor():
+    cupSensor = CupSensorSim()
+    detected = True
+    print("\nPolling for joystick press..")
+    count = 10
+    while count > 0:
+        print(count)
+        count = count - 1
+        distance = cupSensor.getDistance()
+        if distance <= 3:
+            detected = True
+            break
+        else:
+            detected = False
+        time.sleep(1)
+
+    assert(detected == True)
--- WaterBuddy/Tests/pyTests/simulatorTests/test_fillSystemSim.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('../../../')
-sys.path.append('../../../Simulators')
-from fillSystemSim import FillSystemSim
-
-def test_init():
-    pass
